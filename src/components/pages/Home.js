9151b335fa8fabf4a2c8dc7c959c8cb87fc2616d
miranraznatovic <miran.raznatovic@edu.fit.ba>
2022-02-25 06:18:28 +0100

    removing unnecessary code

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,10 @@
 import React from "react";
 import "../../App.css";
 import HeroSection from "../heroSection/HeroSection";
-import Social from "../social/Social";
 
 function Home() {
   return (
-    <>
       <HeroSection />
-      {/* <Social/> */}
-    </>
   );
 }
 export default Home;
