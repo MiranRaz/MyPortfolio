06f8a924a60995167317805836ad27e1cefac76a
miranraznatovic <miran.raznatovic@edu.fit.ba>
2022-03-06 15:33:48 +0100

    responsive page skeleton

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,13 +5,12 @@ export default function Contact() {
   return (
     <div className="contact" id="contact">
       <div className="main">
-        <h2>CONTACT</h2>
+        <h1>CONTACT</h1>
         <div className="page">
           <div className="pageHolder">
             <p>
-              Thank you for checking out my portfolio page you can contact me
-              via <a href="mailto: miran.raznatovic@gmail.com"> email </a> 
-              or my social media pages.
+              My email is{" "}
+              <a href="mailto: miran.raznatovic@gmail.com"> here </a>.
             </p>
           </div>
         </div>
