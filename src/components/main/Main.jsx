f29ec54f2280066b9e316be821cfdae3a7a19156
miranraznatovic <miran.raznatovic@edu.fit.ba>
2022-03-08 08:49:00 +0100

    created portfolio page

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,19 +3,19 @@ import "./Main.scss";
 
 import Navbar from "../navbar/Navbar";
 import SideMenu from "../sidemenu/SideMenu";
-//import HomePage from "../homepage/Homepage";
-import Portfolio from "../portfolio/Portfolio";
+import HomePage from "../homepage/Homepage";
+//import Portfolio from "../portfolio/Portfolio";
 import Footer from "../footer/Footer";
 
 export default function Main() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="mainContainer" id="main">
+    <div className="mainContainer">
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <SideMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      {/* <HomePage /> */}
-      <Portfolio />
+      <HomePage />
+      {/* <Portfolio /> */}
       <Footer />
     </div>
   );
