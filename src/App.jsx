cd585d21d4b1a19c6751244ff3416bcb6007440d
miranraznatovic <miran.raznatovic@edu.fit.ba>
2022-03-08 11:29:15 +0100

    added temperary screen loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import "./App.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Main from "./components/main/Main";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
   return (
     <div className="app">
-      <Main />
+      {loading ? (
+        <div className="loadingData">
+          <h1>Miran Ražnatović</h1>
+        </div>
+      ) : (
+        <Main />
+      )}
     </div>
   );
 }
